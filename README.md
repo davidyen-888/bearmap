17a94ef68b3837d9dec2360ff10ebad640562d4e
David Hsieh <61106644+davidyen-888@users.noreply.github.com>
2021-07-23 20:38:49 +0800

    Organize repo

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# UCB-CS61B
-#### @source: https://sp19.datastructur.es/index.html
-#### @source: https://github.com/Berkeley-CS61B/skeleton-sp19
+# Bearmap
+
--- lab1/HelloNumbers.java
+++
@@ -1,9 +0,0 @@
-public class HelloNumbers {
-    public static void main(String[] args) {
-        int sum = 0;
-        for (int i = 0; i < 10; i++) {
-            sum += i;
-            System.out.println(sum);
-        }
-    }
-}
